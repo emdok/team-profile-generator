954f17aeebac11669e47bca53e10ecde83c58841
Emily Dokken <dokkenemily@gmail.com>
2022-01-16 20:17:55 -0800

    add tests for employee.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,21 +1,43 @@
-class Employee { 
-    constructor(name, id, email) {
+const Employee = require('../lib/Employee.js');
 
-    }
+test('Can I get employee name?', () => {
+    const employee = new Employee('Emily');
 
-    getName() {
+    expect(employee.name).toEqual("Emily");
+});
 
-    }
+test('Can I get employee Id?', () => {
+    const employee = new Employee('Emily', '123', 'test@example.com');
 
-    getId() {
+    expect(employee.id).toEqual("123");
+});
 
-    }
+test('Can I get employee email?', () => {
+    const employee = new Employee('Emily', '123', 'test@example.com');
 
-    getEmail() {
+    expect(employee.email).toEqual("test@example.com");
+});
 
-    }
+test('Does getName Method get the name?', () => {
+    const employee = new Employee('Emily', '123', 'test@example.com');
 
-    getRole() {
-        return Employee;
-    }
-}
+    expect(employee.getName()).toEqual('Emily');
+});
+
+test('Does getId Method get the Id?', () => {
+    const employee = new Employee('Emily', '123', 'test@example.com');
+
+    expect(employee.getId()).toEqual('123');
+});
+
+test('Does getEmail Method get the email?', () => {
+    const employee = new Employee('Emily', '123', 'test@example.com');
+
+    expect(employee.getEmail()).toEqual('test@example.com');
+});
+
+test('Does getRole Method get the Role?', () => {
+    const employee = new Employee('Emily', '123', 'test@example.com');
+
+    expect(employee.getRole()).toEqual('Employee');
+});
